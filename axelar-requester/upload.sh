4e0c3a9440bde60cdc5feadf3018c02dc9812a3d
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-09-10 13:43:29 +0700

    change name

--- axelar-requester/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-LAMBDA_FUNC_NAME=axelar-requester
-PROJECT_PATH=~/Desktop/axelar/lambda/${LAMBDA_FUNC_NAME}
-
-cd ${PROJECT_PATH}
-zip -r ${LAMBDA_FUNC_NAME}.zip .
-aws lambda update-function-code --function-name ${LAMBDA_FUNC_NAME} --zip-file fileb://${PROJECT_PATH}/${LAMBDA_FUNC_NAME}.zip --region us-west-1
-rm ${LAMBDA_FUNC_NAME}.zip
