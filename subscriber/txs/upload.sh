5162899ffeedff89fd0b04a89fc09e3d90f2357d
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-09-11 23:10:27 +0700

    handle network

--- subscriber/txs/upload.sh
+++ subscriber/txs/upload.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-PROJECT_PATH=~/Desktop/axelar/lambda/subscriber
-PROJECT_DIR=txs
-SERVER_USERNAME="root"
-SERVER_IP="128.199.147.16"
-APP_NAME="txs"
+if [ -z "$1" ]
+then
+	NETWORK="testnet"
+else
+	NETWORK=$1
+fi
 
 pkill -f "bash upload.sh"
 
-scp -r ${PROJECT_PATH}/${PROJECT_DIR} ${SERVER_USERNAME}@${SERVER_IP}:~/
+PROJECT_PATH=~/Desktop/axelar/lambda/subscriber
+PROJECT_DIR=txs
+SERVER_USERNAME="root"
+APP_NAME="txs"
 
-ssh ${SERVER_USERNAME}@${SERVER_IP} "pm2 delete ${APP_NAME}"
-ssh ${SERVER_USERNAME}@${SERVER_IP} "pm2 start /${SERVER_USERNAME}/${PROJECT_DIR}/index.js -n ${APP_NAME}"
-ssh ${SERVER_USERNAME}@${SERVER_IP} "pm2 save"
+if [ "${NETWORK}" == "testnet" ] || [ "${NETWORK}" == "mainnet" ]
+then
+	if [ "${NETWORK}" == "testnet" ]
+	then
+		SERVER_IP="128.199.147.16"
+		echo "REQUESTER_API_HOST=https://godeeq5o09.execute-api.us-west-1.amazonaws.com/axelar-testnet" > ${PROJECT_PATH}/${PROJECT_DIR}/.env
+	elif [ "${NETWORK}" == "mainnet" ]
+	then
+		SERVER_IP="128.199.147.16"
+		echo "REQUESTER_API_HOST=https://godeeq5o09.execute-api.us-west-1.amazonaws.com/axelar-testnet" > ${PROJECT_PATH}/${PROJECT_DIR}/.env
+	fi
+
+	scp -r ${PROJECT_PATH}/${PROJECT_DIR} ${SERVER_USERNAME}@${SERVER_IP}:~/
+
+	ssh ${SERVER_USERNAME}@${SERVER_IP} "pm2 delete ${APP_NAME}"
+	ssh ${SERVER_USERNAME}@${SERVER_IP} "pm2 start /${SERVER_USERNAME}/${PROJECT_DIR}/index.js -n ${APP_NAME}"
+	ssh ${SERVER_USERNAME}@${SERVER_IP} "pm2 save"
+fi
