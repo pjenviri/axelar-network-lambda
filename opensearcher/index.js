316a00f39c393d31affb28519db90ee05e5dee9b
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-09-17 01:37:05 +0700

    handle array

--- opensearcher/index.js
+++ opensearcher/index.js
@@ -70,7 +70,7 @@ exports.handler = async (event, context, callback) => {
     objectFields.forEach(objectField => {
       if (body[objectField]) {
         try {
-          body[objectField] = Array.isArray(body[objectField]) ? JSON.parse(body[objectField]) : normalizeObject(JSON.parse(body[objectField]));
+          body[objectField] = body[objectField] && body[objectField].startsWith('[') && body[objectField].endsWith(']') ? JSON.parse(body[objectField]) : normalizeObject(JSON.parse(body[objectField]));
         } catch (err) {}
       }
     });
